2261e0c1e8d736c044bf668bcd17401d44db8dc1
abrormamurov <mabrorbk@gmail.com>
2024-03-07 16:20:23 +0500

    add

--- app.js
+++ app.js
@@ -120,3 +120,53 @@ const evenNumbers = number.filter((num) => {
   return num % 2 !== 0;
 });
 console.log(oddNumbers, evenNumbers);
+
+let products = [
+  {
+    id: 6,
+    name: "Smarthpone",
+    price: 12000,
+    rating: 4.5,
+    discount: 20,
+  },
+  {
+    id: 2,
+    name: "Acer",
+    price: 10000,
+    rating: 4.3,
+    discount: 10,
+  },
+  {
+    id: 1,
+    name: "Mac book",
+    price: 17000,
+    rating: 4.7,
+    discount: 40,
+  },
+  {
+    id: 4,
+    name: "HP",
+    price: 21000,
+    rating: 4.1,
+    discount: 30,
+  },
+  {
+    id: 5,
+    name: "Dell",
+    price: 35000,
+    rating: 4.9,
+    discount: 30,
+  },
+];
+
+const byProduc = products.filter((product) => {
+  return product.id !== 4;
+});
+
+console.log(byProduc);
+
+const fruits = "jcwigyugc9cwwocn".split("").every((letter) => {
+  return !Number(letter);
+});
+
+console.log(fruits);
